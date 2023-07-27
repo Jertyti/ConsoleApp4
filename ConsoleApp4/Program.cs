31fe4458330089ae407884158210e60c578fbbda
DIRSON <dgutsal221@gmail.com>
2023-07-27 23:03:01 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringNumber = "256";
+            int numberFromString = Convert.ToInt32(stringNumber);
+            int square = numberFromString * numberFromString;
+            Console.WriteLine("Квадрат введенного числа равен " + square);
+        }
+    }
+}
